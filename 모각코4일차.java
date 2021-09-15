eed7ed1291b8e3085198470f15edd2cbaeed8976
scarletYewon <kwn09032@gmail.com>
2021-09-15 23:51:44 +0900

    Create 모각코4일차.java

---
+++ 모각코4일차.java
@@ -0,0 +1,17 @@
+package hello;
+
+public class 모각코4일차 {
+
+	public static void main(String[] args) {
+		String name = "코뮤니티";
+		System.out.println(name);
+		int a = 1;
+		System.out.println(a);
+		boolean b = true;
+		System.out.println(b);
+		float c = 0.1f;
+		System.out.println(c);
+		double d = 0.2;
+		System.out.println(d);
+	}
+}
