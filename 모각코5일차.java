5f8d3092b595338078598cdca8fca89fe9f64704
scarletYewon <kwn09032@gmail.com>
2021-09-15 23:51:47 +0900

    Create 모각코5일차.java

---
+++ 모각코5일차.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class ¸ð°¢ÄÚ5ÀÏÂ÷ {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 30;
+		System.out.println("µ¡¼À–E¼À°ö¼À");
+		System.out.print("¿¹½Ã)");
+		System.out.printf("a+b´Â %d ,a-b´Â %d, a*b´Â %d", a+b, a-b, a*b);
+	}
+}
