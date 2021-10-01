a0619ba656820d1456c47a88d7927b6de4533e40
scarletYewon <kwn09032@gmail.com>
2021-10-01 14:59:15 +0900

    Create 모각코15일차.java

---
+++ 모각코15일차.java
@@ -0,0 +1,23 @@
+package hello;
+class NumberThread extends Thread { 
+	public void run() {
+		for (int i = 0; i < 50; i++) {
+			System.out.print(i);
+		}
+	}
+}
+class CharThread extends Thread {
+	public void run() {
+		for (char i = 'a'; i <= 'z'; i++) {
+			System.out.print(i);
+		}
+	} 
+}
+public class 모각코15일차 {
+	public static void main(String[] args) {
+		Thread thread1 = new NumberThread();
+		Thread thread2 = new CharThread();
+		thread1.start();
+		thread2.start();
+	}
+}
